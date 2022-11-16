d53d0bb2203a6e70e002c4606482148f96521ca9
AndrzejFFGrabowski <Andrzej.f.f.Grabowski@gmail.com>
2022-11-16 18:21:08 +0100

    1.0 startup

---
+++ BlazorApp2/Startup.cs
@@ -0,0 +1,40 @@
+﻿namespace BlazorApp2
+{
+    public static class Startup
+    {
+        public static WebApplication InitializeApp(String[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            ConfigureServices(builder);
+            var app = builder.Build();
+            Configure(app);
+            return app;
+        }
+        private static void ConfigureServices(WebApplicationBuilder builder)
+        {
+            builder.Services.AddRazorPages();
+            builder.Services.AddServerSideBlazor();
+            builder.Services.AddSingleton<WeatherForecastService>();
+            builder.Services.AddSingleton<PersonService>();
+            builder.Services.AddHttpClient<IPersonService, PersonService>();
+            builder.Services.AddMvc(options => options.EnableEndpointRouting = false);
+        }
+
+        private static void Configure(WebApplication app)
+        {
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseMvcWithDefaultRoute();
+            app.MapBlazorHub();
+            app.MapFallbackToPage("/_Host");
+        }
+    }
+}
